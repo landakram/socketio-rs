93a56eee01258c092656c7482ebbee243c543fd9
Mark Hudnall <me@markhudnall.com>
2020-05-16 20:21:39 -0700

    Clean up with some modules

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,18 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
-use async_trait::async_trait;
 use futures_util::{SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tokio::sync::RwLock;
 use uuid::Uuid;
-use warp::http::StatusCode;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    let socket_store = new_socket_store();
+    let socket_store = crate::store::new();
 
     let base = warp::path("ws");
 
@@ -72,10 +68,10 @@ fn with_state<T: std::marker::Sync + std::marker::Send>(
 
 async fn on_handshake(
     args: HandshakeArgs,
-    socket_store: Arc<impl SocketStore + std::fmt::Debug>,
+    socket_store: Arc<impl crate::store::SocketStore + std::fmt::Debug>,
 ) -> Result<impl warp::Reply, std::convert::Infallible> {
     let sid = Uuid::new_v4().to_string();
-    let sock = new_socket(sid.clone());
+    let sock = crate::socket::new(sid.clone());
     socket_store.set(sid.clone(), sock).await;
     info!("{:?}", socket_store);
     let res = HandshakeResponse {
@@ -93,7 +89,7 @@ struct HandshakeResponse {
 
 async fn on_poll(
     args: SessionArgs,
-    socket_store: Arc<impl SocketStore>,
+    socket_store: Arc<impl crate::store::SocketStore>,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     let sid = args.sid;
     match socket_store.get(sid).await {
@@ -114,38 +110,69 @@ struct PollResponse {
     t: String,
 }
 
-fn on_data(args: SessionArgs, socket_store: Arc<impl SocketStore>) -> String {
+fn on_data(args: SessionArgs, socket_store: Arc<impl crate::store::SocketStore>) -> String {
     "on_data".to_string()
 }
 
-#[async_trait]
-trait SocketStore: Send + Sync {
-    async fn get(&self, sid: String) -> Option<Arc<Socket>>;
-    async fn set(&self, sid: String, socket: Socket);
-}
+mod store {
+    use async_trait::async_trait;
+    use std::collections::HashMap;
+    use std::sync::Arc;
+    use tokio::sync::RwLock;
 
-#[derive(Debug)]
-struct InMemorySocketStore {
-    sockets: RwLock<HashMap<String, Arc<Socket>>>,
-}
-
-#[async_trait]
-impl SocketStore for InMemorySocketStore {
-    async fn get(&self, sid: String) -> Option<Arc<Socket>> {
-        let s = self.sockets.read().await;
-        s.get(&sid).map(|sock| sock.clone())
+    #[async_trait]
+    pub trait SocketStore: Send + Sync {
+        async fn get(&self, sid: String) -> Option<Arc<crate::socket::Socket>>;
+        async fn set(&self, sid: String, socket: crate::socket::Socket);
     }
 
-    async fn set(&self, sid: String, socket: Socket) {
-        let mut s = self.sockets.write().await;
-        s.insert(sid, Arc::new(socket));
+    #[derive(Debug)]
+    struct InMemorySocketStore {
+        sockets: RwLock<HashMap<String, Arc<crate::socket::Socket>>>,
+    }
+
+    #[async_trait]
+    impl SocketStore for InMemorySocketStore {
+        async fn get(&self, sid: String) -> Option<Arc<crate::socket::Socket>> {
+            let s = self.sockets.read().await;
+            s.get(&sid).map(|sock| sock.clone())
+        }
+
+        async fn set(&self, sid: String, socket: crate::socket::Socket) {
+            let mut s = self.sockets.write().await;
+            s.insert(sid, Arc::new(socket));
+        }
+    }
+
+    pub fn new() -> Arc<impl SocketStore + std::fmt::Debug> {
+        Arc::new(InMemorySocketStore {
+            sockets: RwLock::new(HashMap::new()),
+        })
     }
 }
 
-fn new_socket_store() -> Arc<impl SocketStore + std::fmt::Debug> {
-    Arc::new(InMemorySocketStore {
-        sockets: RwLock::new(HashMap::new()),
-    })
+mod socket {
+    #[derive(Debug)]
+    pub struct Socket {
+        pub sid: String,
+        transport: crate::transport::Transport,
+    }
+
+    impl Socket {}
+
+    pub fn new(sid: String) -> Socket {
+        Socket {
+            sid: sid,
+            transport: crate::transport::Transport {},
+        }
+    }
+}
+
+mod transport {
+    #[derive(Debug)]
+    pub struct Transport {}
+
+    impl Transport {}
 }
 
 #[derive(Deserialize, Debug, Eq, PartialEq)]
@@ -178,30 +205,10 @@ struct HandshakeArgs {
     b64: Option<bool>,
 }
 
-#[derive(Debug)]
-struct Transport {}
-
-impl Transport {}
-
-#[derive(Debug)]
-struct Socket {
-    sid: String,
-    transport: Transport,
-}
-
-impl Socket {}
-
-fn new_socket(sid: String) -> Socket {
-    Socket {
-        sid: sid,
-        transport: Transport {},
-    }
-}
-
 async fn handle_conn(
     ws: warp::ws::WebSocket,
     args: UpgradeArgs,
-    socket_store: Arc<impl SocketStore>,
+    socket_store: Arc<impl crate::store::SocketStore>,
 ) {
     let (mut tx, mut rx) = ws.split();
 
